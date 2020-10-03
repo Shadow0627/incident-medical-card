52604e64ab22837b275599648a42de8e0a935dbd
unknown <piorom12@gmail.com>
2020-10-03 18:16:14 +0200

    add include

---
+++ templates/add-pati.php
@@ -0,0 +1,19 @@
+<form method="post">
+        <h1>Dane pacjenta</h1>
+        <label for="napa">Imię pacjenta:</label>
+        <input type="text" name="napa" id="napa" required>
+        <label for="lapa">Nazwisko pacjenta:</label>
+        <input type="text" name="lapa" id="lapa" require>
+        <label for="pnpa">Numer telefonu:</label>
+        <input type="text" name="pnpa" id="pnpa">
+        <label for="empa">Email pacjenta:</label>
+        <input type="email" name="empa" id="empa">
+        <label for="cipa">Miasto: </label>
+        <input type="text" name="cipa" id="cipa">
+        <label for="zcpa">Kod pocztowy:</label>
+        <input type="text" name="zcpa" id="zcpa">
+        <label for="snpa">Ulica i numer domu:</label>
+        <input type="text" name="snpa" id="snpa" required>
+        <input type="submit" value="Zapisz!" name="pati">
+    </form>
+    
--- templates/inci.php
+++ templates/inci.php
@@ -10,24 +10,7 @@
     </ul>
 </header>
 <div id="add" class="inci-add">
-    <form method="post">
-        <h1>Dane pacjenta</h1>
-        <label for="napa">Imię pacjenta:</label>
-        <input type="text" name="napa" id="napa" required>
-        <label for="lapa">Nazwisko pacjenta:</label>
-        <input type="text" name="lapa" id="lapa" require>
-        <label for="pnpa">Numer telefonu:</label>
-        <input type="text" name="pnpa" id="pnpa">
-        <label for="empa">Email pacjenta:</label>
-        <input type="email" name="empa" id="empa">
-        <label for="cipa">Miasto: </label>
-        <input type="text" name="cipa" id="cipa">
-        <label for="zcpa">Kod pocztowy:</label>
-        <input type="text" name="zcpa" id="zcpa">
-        <label for="snpa">Ulica i numer domu:</label>
-        <input type="text" name="snpa" id="snpa" required>
-        <input type="submit" value="Zapisz!" name="pati">
-    </form>
+<?php include '/templates/add-pati.php'; ?>
     <form method="post">
         <h1>Dane wyjazdowe</h1>
         <label for="exco">Kod wyjazdu:</label>
