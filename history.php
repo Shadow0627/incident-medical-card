b70d32bb6a4d9241514450cae242f9e24aa0fe48
Shadow0627 <piorom12@gmail.com>
2020-11-01 22:19:21 +0100

    We are making history !!

--- history.php
+++ history.php
@@ -2,7 +2,9 @@
 include 'config/init.php';
 if(isset($_SESSION['confirm']))
 {
+    include 'templates/head.php';
     include 'templates/sh-all-history.php';
+    include 'templates/footer.php';
 }
 else
 {
--- templates/sh-all-history.php
+++ templates/sh-all-history.php
@@ -2,10 +2,22 @@
 $results = $inci->getalldata($_POST['idin']);
 foreach($results as $row)
 {
- print('Nazwisko pacjenta: '. $row['LAPA'] .'<br>');
- print('Czas wyjazdu/powrotu: '. $row['DDTD']. '/'. $row['BBTD'] .'<br>');
- print('Miejscowość: '. $row['CILO'] .'<br>');
- print('Medyk: '. $row['MENA'] .' '. $row['LAME'] .'<br>');
+print('Dane pacjenta: ');
+ print('Nazwisko pacjenta: '. $row['LAPA']);
+ print('Imię pacjenta: '. $row['NAPA']);
+ print('Numer telefonu pacjenta: '. $row['PNPA']);
+ print('Email pacjenta: '. $row['EMPA']);
+ print('Miejsce zamieszkania pacjenta: '. $row['ZCPA'] .' '. $row['CIPA'] .' '. $row['SNPA']);
+ print('Dane miejsca zdarzenia: ');
+ print('Miejscowość: '. $row['ZCLO'] .' '. $row['CILO'] .' '. $row['STLO']);
+ 
+print('Dane wyjazdu:');
+print('Czas wyjazdu/przybycia/powrotu: '. $row['DDTD']. '/'. $row['ARTD'] .'/'. $row['BBTD'] .'<br>');
+print('Medyk: '. $row['MENA'] .' '. $row['LAME'] .'<br>');
+print('Kod wyjazdu: '.$row['NUEX']);
+print('Pilność wyjazdu: ' .$row['NAUO']);
+print('Typ wezwania: ' .$row['NATO']);
+
 }
 
 ?>
